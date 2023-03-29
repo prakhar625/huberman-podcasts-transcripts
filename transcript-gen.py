4267733626ee167130e29fbba0fe2b86c69d11b2
Prakhar Bhardwaj <prakhar625@users.noreply.github.com>
2023-03-29 21:32:49 +0530

    added huber-bot

--- transcript-gen.py
+++ transcript-gen.py
@@ -4,7 +4,7 @@ from youtube_transcript_api.formatters import TextFormatter
         
 curr_folder = os.path.dirname(os.path.realpath(__file__))
 print(curr_folder)
-directory = "transcripts"
+directory = "transcripts-all"
 path = os.path.join(curr_folder, directory)
 os.mkdir(path)
 print("Directory '% s' created" % directory)
@@ -15,13 +15,13 @@ def gen_trans(video_id, i):
     transcript = YouTubeTranscriptApi.get_transcript(video_id_str, languages=['en-US', 'en'])
     
     # create unique folder inside the transcripts folder, for each video
-    os.mkdir('transcripts/%s' %i)
+    # os.mkdir('transcripts/%s' %i)
     
     #start writing to files, within their own folder for each video id
     print("Writing transcript to text file")
     txt_formatter = TextFormatter()
     txt_formatted = txt_formatter.format_transcript(transcript)
-    with open('transcripts/%s/' %i + '%s__transcript.txt' % i, 'w') as txt_file:
+    with open('transcripts-all/' + '%s__transcript.txt' % i, 'w') as txt_file:
         txt_file.write(txt_formatted)
 
     print("done with video")
